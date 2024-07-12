cc0903544ad88d8519ee4351afaa1061ffc67d37
Govind <you@example.com>
2024-07-12 15:35:44 +0530

    updating_code_and_images

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 8080;
-
 const BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIwNzczMjQyLCJpYXQiOjE3MjA3NzI5NDIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjhmZjY2Nzg3LTYwODctNDc2Ny05Yjg5LWY0MGY1ZDQzMGYyZCIsInN1YiI6ImdvdmluZHVwYWRoeWF5ODUyNzNAZ21haWwuY29tIn0sImNvbXBhbnlOYW1lIjoiQWZmb3JkIE1lZGljYWwiLCJjbGllbnRJRCI6IjhmZjY2Nzg3LTYwODctNDc2Ny05Yjg5LWY0MGY1ZDQzMGYyZCIsImNsaWVudFNlY3JldCI6Ilpyd3N0aEZzYUxKZ1RldkUiLCJvd25lck5hbWUiOiJHb3ZpbmQgVXBhZGh5YXkiLCJvd25lckVtYWlsIjoiZ292aW5kdXBhZGh5YXk4NTI3M0BnbWFpbC5jb20iLCJyb2xsTm8iOiIyMTE1NTAwMDYyIn0.9Htf2SKkewNHWzknhjIp_W8bo783oTCWo_jvL93Vo4s'; 
-
 app.get('/numbers', async (req, res) => {
   try {
     let { url } = req.query; 
-
     if (!Array.isArray(url)) {
       url = [
         "http://20.244.56.144/test/primes", 
@@ -17,9 +14,7 @@ app.get('/numbers', async (req, res) => {
         "http://20.244.56.144/test/odd", 
       ];
     }
-
     const numbers = [];
-
     const requests = url.map(async (url) => {
       try {
         const response = await axios.get(url, {
@@ -33,7 +28,6 @@ app.get('/numbers', async (req, res) => {
         console.error(`Error retrieving numbers from ${url}: ${error.message}`);
       }
     });
-
     await Promise.all(requests);
     const mergedNumbers = Array.from(new Set(numbers)).sort((a, b) => a - b);
     console.log(mergedNumbers);
@@ -44,7 +38,6 @@ app.get('/numbers', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
